6e4236c5f223f7b2ab9a49041dc8864c954f9819
milankecske <139019835+milankecske01@users.noreply.github.com>
2024-10-08 08:58:28 +0200

    added changes via github

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,23 @@
-
-const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
-async function f1 (lat, long, timeStr) {
-    //TODO
+async function f1 (latitude, longitude, date) {
+    let temperature = await fetchWeather(latitude, longitude, date);
+    let sum = 0;
+    temperature.hourly.temperature_2m.forEach((t) => {
+      sum += t;
+    });
+    return Math.floor(sum / temperature.hourly.temperature_2m.length);
 }
-
+async function fetchWeather(latitude, longitude, date) {
+    let url = `https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=${date}&end_date=${date}&hourly=temperature_2m`;
+    try {
+      let data = await fetch(url);
+      if (!data.ok) {
+          alert("Valami nem sikerült")
+        throw new Error(`HTTP error! Status: ${data.status}`);
+      }
+      let output = await data.json().then((output) => output);
+      return output;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 module.exports = f1;
